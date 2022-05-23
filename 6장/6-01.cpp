b4d21e2f1812ad38a373de613168541d0880a11d
ljh3648 <ljh09060@gmail.com>
2022-05-23 23:28:25 +0900

    Create 6-01.cpp

---
+++ 6장/6-01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int b = 4;
+
+    int c = a + b;
+    int d = a - b;
+    int e = a * b;
+    int f = a / b;
+
+    int g = a % b;
+
+    cout << "8 + 4 = " << c << "\n";
+    cout << "8 - 4 = " << d << "\n";
+    cout << "8 * 4 = " << e << "\n";
+    cout << "8 / 4 = " << f << "\n";
+    cout << "8 % 4 = " << g << "\n";
+
+    return 0;
+}
