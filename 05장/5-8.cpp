e4bd4dc4a9893f13721640ac3440ffbde4bab834
ljh3648 <ljh09060@gmail.com>
2022-05-23 22:32:35 +0900

    Create 5-8.cpp
    bool type

---
+++ 05장/5-8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool b1 = true, b2 = false;
+    cout << "b1 = " << b1 << "\n";
+    cout << "b2 = " << b2 << "\n";
+
+    return 0;
+}
